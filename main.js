6312e2bcea099dce8ff168210ba6f24086ad9be7
AdryDev <adrydevmateo@gmail.com>
2024-06-15 15:06:26 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const form = document.getElementById('form')
+const current_date = new Date()
+
+form.addEventListener('change', (e) => {
+ e.preventDefault()
+
+ console.log('Element: ', e.target);
+})
+
+// Day
+
+// Month
+
+// Year
+
